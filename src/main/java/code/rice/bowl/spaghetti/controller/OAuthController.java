ee9d5ed4419bbdeb9b6b3a7027e2b21d09b28bad
BieNew22 <newbie11004@gmail.com>
2025-03-27 23:23:56 +0900

    Feat: add google login logic in controller

--- src/main/java/code/rice/bowl/spaghetti/controller/OAuthController.java
+++ src/main/java/code/rice/bowl/spaghetti/controller/OAuthController.java
@@ -1,6 +1,10 @@
 package code.rice.bowl.spaghetti.controller;
 
+import code.rice.bowl.spaghetti.dto.JwtTokenDto;
+import code.rice.bowl.spaghetti.dto.LoginProvider;
+import code.rice.bowl.spaghetti.dto.request.LoginRequest;
 import code.rice.bowl.spaghetti.dto.response.GoogleTokenResponse;
+import code.rice.bowl.spaghetti.service.AuthService;
 import code.rice.bowl.spaghetti.service.GoogleLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuthController {
 
     private final GoogleLoginService googleLoginService;
+    private final AuthService authService;
 
     @GetMapping("/google")
     public ResponseEntity<?> googleLogin(@RequestParam String token) {
         // 1. access token 발급.
         GoogleTokenResponse accessToken = googleLoginService.getAccessToken(token);
 
+        LoginRequest request = new LoginRequest(accessToken.getAccess_token(),
+                LoginProvider.GOOGLE);
 
-        return ResponseEntity.ok(accessToken);
+        // 2. 로그인 로직 처리
+        JwtTokenDto jwt = authService.login(request);
+
+        // 3. 반환
+        return ResponseEntity.ok(jwt);
     }
 }
