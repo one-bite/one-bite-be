be513f8d6c19d4deb7b29ea71525e6aaf55e825d
BieNew22 <newbie11004@gmail.com>
2025-03-28 11:25:05 +0900

    Docs: add comment

--- src/main/java/code/rice/bowl/spaghetti/exception/InvalidRequestException.java
+++ src/main/java/code/rice/bowl/spaghetti/exception/InvalidRequestException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * return for 400 status
+ * 사용자의 잘 못된 요청에 대한 에러, return for 400 status
  */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidRequestException extends RuntimeException{
