1547afc1ff7e5480a79dcf62f5d4527cf3c8fbd1
BieNew22 <newbie11004@gmail.com>
2025-04-17 20:26:30 +0900

    Refactor: change file name (CurrentUser -> UserCurrent) & move to user (UserPatchReq: request -> user)

--- src/main/java/code/rice/bowl/spaghetti/dto/user/UserPatchRequest.java
+++ src/main/java/code/rice/bowl/spaghetti/dto/user/UserPatchRequest.java
@@ -1,8 +1,12 @@
-package code.rice.bowl.spaghetti.dto.request;
+package code.rice.bowl.spaghetti.dto.user;
 
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 용도
+ * - 사용자 정보 업데이트 (현재 : 닉네임 변경)
+ */
 @Getter
 @Setter
 public class UserPatchRequest {
