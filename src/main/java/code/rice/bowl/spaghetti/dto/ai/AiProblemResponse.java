4ff81e96ae0920ec412e1af6509c33132de23706
heung115 <heung115@naver.com>
2025-05-04 16:31:31 +0900

    feat: add comments in AiProblemResponse

--- src/main/java/code/rice/bowl/spaghetti/dto/ai/AiProblemResponse.java
+++ src/main/java/code/rice/bowl/spaghetti/dto/ai/AiProblemResponse.java
@@ -4,6 +4,11 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/*
+ * ai api에서 보내주는 정보를 받아오기 위해서 설계했으나.
+ * 현재는 그냥 ProblemRequest을 사용중이라 사용되는 곳이 없음.
+ * 후에 상황보고 완전 필요없어지면 삭제할 예정
+ */
 @Getter
 @NoArgsConstructor
 public class AiProblemResponse {
